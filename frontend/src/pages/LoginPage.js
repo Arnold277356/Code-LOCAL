5dd543ab8fb195356e38075631d6414d6c16b242
Arnold277356 <arnoldjensen11@gmail.com>
2026-01-11 23:54:03 +0800

    whatever man

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -24,7 +24,7 @@ function LoginPage() {
         body: JSON.stringify({ email, password }),
       });
 
-      const data = awaitresponse.json();
+      const data = await response.json();
 
       if (response.ok) {
         localStorage.setItem('user', JSON.stringify(data.user));
